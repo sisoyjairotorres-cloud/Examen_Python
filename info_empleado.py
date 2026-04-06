fd031034ca8e5eafa0d4b5c85abda800aa57c957
sisoyjairotorres-cloud <sisoyjairotorres@gmail.com>
2026-04-06 21:39:39 +0000

    Segundo commit

--- info_empleado.py
+++ info_empleado.py
@@ -14,3 +14,26 @@ datos_empleado(nombre,cargo,añosexp)
 #ejemplos
 datos_empleado("mauricio","programador ",4)
 datos_empleado("paul","Dev",7)
+
+class NetworkDevice:
+    def __init__(self, name,ip_address,device_type):
+        self.name = name
+        self.ip_address = ip_address
+        self.device_type = device_type
+
+    def showinfo(self):
+            mi_name = self.name
+            mi_ip_address = self.ip_address
+            mi_device_type = self.device_type
+
+            print("Diospositivo: <" 
+                + str(mi_name) + "> | IP: <" + mi_ip_address + "> | Tipo: <"
+                + str(mi_device_type) + ">")
+dev1= NetworkDevice("Router1","10.0.0.1","Router")
+dev2= NetworkDevice("Switch","10.0.0.2","Switch")
+dev3= NetworkDevice("Firewall","10.0.0.3","Firewall")
+
+dev1.showinfo()
+dev2.showinfo()
+dev3.showinfo()
+
